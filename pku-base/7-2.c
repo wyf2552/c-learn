fab5c3d1246e3eae870500c4525c38a5f0dd3a3e
wyf2552 <wyf5322@gmail.com>
2024-02-10 22:38:09 +0800

    add descriptions

--- pku-base/7-2.c
+++ pku-base/7-2.c
@@ -1,31 +1,72 @@
 #include <stdio.h>
-int n ;
-int ret;
+
+/* 描述
+
+输入一个0--100的分数，判断分数代表什么等级。
+
+95<=分数<=100, 输出1
+
+90<=分数<95,输出2
+
+85<=分数<90,输出3
+
+80<=分数<85,输出4
+
+70<=分数<80,输出5
+
+60<=分数<70输出6
+
+分数 < 60;输出7.
+
+输入
+
+n
+
+输出
+
+m
+
+样例输入
+
+1
+87
+
+样例输出
+
+1
+3 */
+
 int main()
 {
+    int n ;
+    int ret;
+
     scanf("%d", &n);
-    if (n >= 95 && n <= 100) {
-        ret = 1;
-    } else if (n >= 90 && n < 95) {
-        ret = 2;
-    } else if (n >= 85 && n < 90) {
-        ret = 3;
-    } else if (n >= 80 && n < 85) {
-        ret = 4;
+
+    switch (n) {
+        case 95 ... 100:
+            ret = 1;
+            break;
+        case 90 ... 94:
+            ret = 2;
+            break;
+        case 85 ... 89:
+            ret = 3;
+            break;
+        case 80 ... 84:
+            ret = 4;
+            break;
+        case 70 ... 79:
+            ret = 5;
+            break;
+        case 60 ... 69:
+            ret = 6;
+            break;
+        default :
+            ret = 7;
     }
-    else if ( n >= 70 && n < 80)
-    {
-        ret = 5;
-    }
-    else if ( n >= 60 && n <70)
-    {
-        ret = 6;
-    }
-    else if ( n < 60)
-    {
-        ret = 7;
-    }
+
     printf ("%d\n", ret);
-    return 0;
+    
 }
 
