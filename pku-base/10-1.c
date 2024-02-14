50a80af3289cce1e7eeb55562edac32f11d75767
wyf2552 <wyf5322@gmail.com>
2024-02-14 23:29:07 +0800

    10-

--- pku-base/10-1.c
+++ pku-base/10-1.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+/* 描述
+
+在一个字符串中找出元音字母a,e,i,o,u出现的次数。
+
+输入
+
+输入一行字符串（字符串中可能有空格，请用cin.getline(s,counts)
+方法把一行字符串输入到字符数组s中，其中counts是s的最大长度，这道题里面可以直接写80。），
+字符串长度小于80个字符。
+
+输出
+
+输出一行，依次输出a,e,i,o,u在输入字符串中出现的次数，整数之间用空格分隔。
+
+样例输入
+
+If so, you already have a Google Account. You can sign in on the right.
+
+样例输出
+
+5 4 3 7 3 */
+
 int main() {
     char str[80] = {0};
     int sum[5] = {0};
